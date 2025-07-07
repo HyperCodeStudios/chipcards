11a64e3ef37a9df4e1dd4afa7131becfeab5dc8c
HyperCodeStudios <105921524+HyperCodeStudios@users.noreply.github.com>
2025-07-07 12:13:06 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,44 +5,54 @@
 
 #define GPIO_DOOR_CONTROL GPIO_NUM_25 // Türsteuerung GPIO-Pin
 
+#define UART_BAUD_RATE 9600
+#define GPIO_RX GPIO_NUM_16
+#define GPIO_TX GPIO_NUM_17
+
+#define STATE_IDLE 0
+#define STATE_AWAIT 1
+#define STATE_ACCEPT 2
+#define STATE_DECLINE 3
+
+HardwareSerial uart(2);
+int state = STATE_IDLE;
+
 void setup()
 {
     Serial.begin(9600); // Serielle Kommunikation starten
     pinMode(GPIO_DOOR_CONTROL, OUTPUT); // Türsteuerung als Ausgang konfigurieren
     digitalWrite(GPIO_DOOR_CONTROL, HIGH); // Tür schließen
-    neopixel_setup(); // Neopixel initialisieren
-    neopixel_off();
-    rfid_setup(); // RFID initialisieren
     http_setup(); // HTTP initialisieren
+    uart.begin(UART_BAUD_RATE, SERIAL_8N1, GPIO_RX, GPIO_TX);
 }
 
 void loop()
 {
-    String uid = read_rfid(); // RFID-Karte lesen und UID erhalten
-    if(uid != "")
+    String message = "";
+    while(uart.available() > 0)
     {
-        neopixel_white(); // Neopixel auf weiß setzen (Karte gelesen)
-        Serial.println("OPENING DOOR!"); // Nachricht auf der seriellen Konsole ausgeben
-        digitalWrite(GPIO_DOOR_CONTROL, LOW); // Tür öffnen
-        neopixel_green(); // Neopixel auf grün setzen (Zugang gewährt)
-        delay(5000); // 5 Sekunden warten, Tür bleibt offen
-        digitalWrite(GPIO_DOOR_CONTROL, HIGH); // Tür schließen
-        neopixel_off(); // Neopixel ausschalten
-        /*if(http_check_id(uid)) // UID mit HTTP-Anfrage überprüfen
+        message += uart.read();
+    }
+    switch(state)
+    {
+    case STATE_IDLE:
+        if(message == "") break;
+        if(!message.startsWith("DATA: ")) break;
+        if(http_check_id(uid)) // UID mit HTTP-Anfrage überprüfen
         //if(uid == "73AB8AF5")
         {
             Serial.println("OPENING DOOR!"); // Nachricht auf der seriellen Konsole ausgeben
+            state = STATE_ACCEPT;
             digitalWrite(GPIO_DOOR_CONTROL, LOW); // Tür öffnen
-            neopixel_green(); // Neopixel auf grün setzen (Zugang gewährt)
+            uart.println("ACCEPT")
             delay(5000); // 5 Sekunden warten, Tür bleibt offen
             digitalWrite(GPIO_DOOR_CONTROL, HIGH); // Tür schließen
-            neopixel_off(); // Neopixel ausschalten
         }
         else // UID ist nicht berechtigt
         {
-            neopixel_red(); // Neopixel auf rot setzen (Zugang verweigert)
+            uart.println("DECLINE")
             delay(3000); // 3 Sekunden warten
-            neopixel_off(); // Neopixel ausschalten
-        }*/
+        }
+        break;
     }
 }
